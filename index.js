15239989486155ef065970d17b79671b3aeda103
Elliot Thiebaut <hello@elliotthiebaut.com>
2021-03-13 01:33:48 +0100

    :heavy_plus_sign: Added dotenv

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World 🌍");
+const Discord = require('discord.js');
+
+require("dotenv").config();
+const client = new Discord.Client();
+client.login(process.env.BOT_TOKEN);
+
+
+client.on('ready', () => {
+    console.log("🌈 Ready to roll 🌈");
+});
+
+client.on('message', (msg) => {
+    if (msg.content === 'issou') {
+        msg.reply('Issouuuuuu')
+    }
+})
