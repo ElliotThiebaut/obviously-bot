8cb6d770a6d4b67a1211741348d2ce6c8cd4c1f3
Elliot Thiebaut <hello@elliotthiebaut.com>
2021-03-13 02:47:38 +0100

    :art: Added command handler

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const Discord = require('discord.js');
-const axios = require('axios');
+const commandHandler = require('./commands')
 
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
@@ -10,22 +10,4 @@ client.on('ready', () => {
     console.log("🚀 Ready to roll 🚀");
 });
 
-client.on('message', async (msg) => {
-
-    let args = msg.content.split(' ')
-
-    if (args[0] === '!gif') {
-        let keywords = 'risitas';
-        if (args.length > 1) {
-            keywords = args.slice(1, args.length).join(" ");
-        }
-
-        let url = `https://g.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}&contentfilter=off`;
-        
-        let response = await axios.get(url);
-        let data = response.data;
-        let index = Math.floor(Math.random() * data.results.length)
-
-        msg.channel.send(data.results[index].url);
-    }
-})
+client.on('message', commandHandler)
---
+++ commands/issou.js
@@ -0,0 +1,3 @@
+module.exports = (msg, args) => {
+    msg.reply('Issou !')
+}
