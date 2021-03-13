ecff6f83047f9f76fbc3c4006544291e0a2db8a5
Elliot Thiebaut <hello@elliotthiebaut.com>
2021-03-13 01:46:17 +0100

    :heavy_plus_sign: Added axios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const Discord = require('discord.js');
-
 require("dotenv").config();
+const Discord = require('discord.js');
+const axios = require('axios');
+
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
 
@@ -9,8 +10,12 @@ client.on('ready', () => {
     console.log("🌈 Ready to roll 🌈");
 });
 
-client.on('message', (msg) => {
+client.on('message', async (msg) => {
     if (msg.content === 'issou') {
         msg.reply('Issouuuuuu')
+    } else if (msg.content === '&gif') {
+        
+        let url = `https://g.tenor.com/v1/search?q=risitas&key=${process.env.TENOR_KEY}&limit=8`
+        
     }
 })
