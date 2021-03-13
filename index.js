03806514329ca0d4674797e2ba07c03530a6ce9a
Elliot Thiebaut <hello@elliotthiebaut.com>
2021-03-13 02:10:24 +0100

    :sparkles: New !gif commande

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ client.on('ready', () => {
 });
 
 client.on('message', async (msg) => {
-    if (msg.content === 'issou') {
-        msg.reply('Issouuuuuu')
-    } else if (msg.content === '&gif') {
+
+    let args = msg.content.split(' ')
+
+    if (args[0] === '!gif') {
+        let keywords = 'risitas';
+        if (args.length > 1) {
+            keywords = args.slice(1, args.length).join(" ");
+        }
+
+        let url = `https://g.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}&contentfilter=off`;
         
-        let url = `https://g.tenor.com/v1/search?q=risitas&key=${process.env.TENOR_KEY}&limit=8`
-        
+        let response = await axios.get(url);
+        let data = response.data;
+        let index = Math.floor(Math.random() * data.results.length)
+
+        msg.channel.send(data.results[index].url);
     }
 })
